a9dd859686cea302916422c7db678d092524b455
Devarsh Patel <121656558+Devarsh2241@users.noreply.github.com>
2023-08-04 22:49:18 +0530

    Add files via upload

---
+++ Angular-Practice/Color-Effect/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { LowerCasePipe } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  color='';
+
+  colour(box:any){
+    this.color=box
+    }
+  
+ 
+}
